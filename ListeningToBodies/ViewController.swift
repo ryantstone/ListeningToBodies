cd48a5b200c5a443c0893ca099f44568b746ef45
Jim Slusser <jslusser@gmail.com>
2019-01-14 11:57:11 -0500

    Initial Commit

---
+++ ListeningToBodies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListeningToBodies
+//
+//  Created by James Slusser on 1/14/19.
+//  Copyright © 2019 James Slusser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
