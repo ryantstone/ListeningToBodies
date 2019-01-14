9a871f404693d892128d946986a58a690fa1ac5d
Jim Slusser <jslusser@gmail.com>
2019-01-14 16:42:32 -0500

    Loaded additional content

---
+++ ListeningToBodies/PrefaceViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PrefaceViewController.swift
+//  ListeningToBodies
+//
+//  Created by James Slusser on 1/14/19.
+//  Copyright © 2019 James Slusser. All rights reserved.
+//
+
+import UIKit
+
+class PrefaceViewController: UIViewController {
+
+    @IBAction func bsClicked(_ sender: UIButton) {
+    }
+    @IBAction func bpClicked(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
